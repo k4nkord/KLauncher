0648b6f8b025680acd17d225eb4d9fdc9001fe48
drunkjank <russian.hacker.shrug@goolemail.com>
2024-02-11 15:58:01 +0300

    fuck it

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
-import 'src/khomescreen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'src/khomescreen.dart';
 
-void main() async {
-  runApp(const KApp());
+void main() => runApp(const KApp());
+
+class KApp extends StatefulWidget {
+  const KApp({super.key});
+
+  @override
+  State<KApp> createState() => _KAppState();
 }
 
-class KApp extends StatelessWidget {
-  const KApp({super.key});
+class _KAppState extends State<KApp> {
+  bool darkTheme = true;
+  Color seedColor = Colors.white;
+  Color secondaryColor = const Color(0xff222222);
+  bool initialized = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: const KHomeScreen(),
         theme: ThemeData(
-            colorScheme: const ColorScheme.dark(background: Colors.black),
-            disabledColor: const Color(0xff333333),
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: seedColor,
+              background: darkTheme ? Colors.black : Colors.white, 
+              secondary: secondaryColor),
             textTheme: TextTheme(
                 bodyMedium: GoogleFonts.exo2(
-                    fontSize: 18, fontWeight: FontWeight.w500)),
-            textButtonTheme: TextButtonThemeData(
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white)),
-            floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              backgroundColor: Color(0xff111111),
-              foregroundColor: Colors.white,
-            ),
-            elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xff111111),
-              foregroundColor: Colors.white,
-            ))));
+                    fontSize: 18, fontWeight: FontWeight.w500),
+                bodySmall: GoogleFonts.exo2(
+                  fontSize: 18, fontWeight: FontWeight.w500, color: secondaryColor
+                )),
+            ));
+  }
+
+  @override
+  initState() {
+    super.initState();
+    if (!initialized) {
+      _init();
+    }
+  }
+
+  void _init() async {
+    var prefs = await SharedPreferences.getInstance();
+
+    prefs.setBool('darkTheme', false);
+    prefs.setInt('seedColor', 0xff0000ff);
+    prefs.setInt('secondaryColor', 0xffff0000);
+
+    darkTheme = prefs.getBool('darkTheme') ?? darkTheme;
+    seedColor = Color(prefs.getInt('seedColor') ?? seedColor.value);
+    secondaryColor = Color(prefs.getInt('secondaryColor') ?? secondaryColor.value);
+    setState(() {
+      initialized = true;
+    });
   }
 }
